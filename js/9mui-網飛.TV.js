2683e87d844dfb0aa27a9dbd9fe425001effe35d
ninemui <ninemui@gmail.com>
2025-01-11 22:29:20 -0800

    update

--- js/9mui-網飛.TV.js
+++ js/9mui-網飛.TV.js
@@ -3,7 +3,7 @@ muban.mxpro.二级.tabs = '#y-playList&&.tab-item'
 var rule={     
     title:'网飞.TV',
     模板:'mxpro',
-    host:'https://www.wangfei.io',
+    host:'https://www.wangfei1.cc',
     // url:'/vodshow/id/fyclass/page/fypage.html',
     url:'/vodshow/id/fyfilter.html',
     filterable:1,//是否启用分类筛选,
